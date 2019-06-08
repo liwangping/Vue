2da3c4c8f029f219f5fd67dc6ae792e525fd999e
name <lwpwyf@qq.com>
2019-06-08 12:38:00 +0800

    Vue-demo

--- Vue-ivewB/Vue-ivewB/src/main.js
+++ Vue-ivewB/Vue-ivewB/src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview'
+import echarts from 'echarts' //引入echarts
 import 'iview/dist/styles/iview.css'
 
+
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
