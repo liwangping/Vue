df1b124599c69c4276b5d6ab9c5b29b0628d2b0a
name <lwpwyf@qq.com>
2019-06-09 20:26:36 +0800

    AwesomePost_B

---
+++ AwesomePost_B/src/components/common/leftNav.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-goumai"></i>
+                <div class="">收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dianpu"></i>
+                <div class="">店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-hanbao"></i>
+                <div class="">商品</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-huiyuanqia"></i>
+                <div class="">会员</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-liebiao"></i>
+                <div class="">统计</div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'leftNav',
+    data () {
+        return {
+             
+        }
+    }
+}
+</script>
+
+<style>
+    .left-nav{
+       color:#fff;
+       font-size:10px;
+       height:100%;
+       background-color: #1D8ce0;
+       float:left;
+       width:5%;
+    }
+    .iconfont{
+       font-size:24px;
+    }
+    .left-nav ul{
+        padding:0px;
+        margin: 0px;
+    }
+    .left-nav li{
+        list-style: none;
+        text-align: center;
+        border-bottom:1px solid #20a0ff;
+        padding:10px;
+    }
+</style>
+
