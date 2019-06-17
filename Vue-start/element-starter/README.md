3ef2002f8c1542e2f02e56c1761f47d63f9cb7ff
name <lwpwyf@qq.com>
2019-06-17 23:28:43 +0800

    Vue-start

---
+++ Vue-start/element-starter/README.md
@@ -0,0 +1,20 @@
+- 快速开发
+ 选择合适starter
+ .git 删除
+
+- 全家桶
+vue + vue-router + vuex + axios 
+
+- 先配vue-router 路由接管一切
+./router/index.js   index.js会补齐
+
+- pages/
+所有路由级别组件，都叫页面组件
+alias一下
+
+- less less-loader 
+    scoped 当前组件
+- 更酷
+  transition的
+  v-enter(短) -> v-enter-active(transtion时间)
+  v-leave(短) -> v-leave-active(opacity:0，transtion时间)
---
+++ Vue-start/element-starter/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import {
+    login
+} from 'pages/';
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path:'/',
+        name: '登录',
+        hidden: true,
+        component: login
+    }
+]
+
+export default new VueRouter({
+    routes,
+    // 设置环境
+    // strict: process.env.NODE_ENV !== 'production'
+})
+
