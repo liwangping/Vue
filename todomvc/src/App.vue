c537e0b9c96a3b012c8f4815fcf41f3dbaf5eb3f
name <lwpwyf@qq.com>
2019-06-17 23:30:03 +0800

    todomvc

---
+++ todomvc/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <counter />
+    </div>
+</template>
+
+<script>
+// 两个兄弟组件，共享count数据
+// count不应该属于Counter组件私有,data(){},props
+import Counter from './components/Counter'
+// import CounterBrother from './components/CounterBrother'
+export default {
+    data(){
+        return {
+            count: 0
+        }
+    },
+    methods:{
+        // incrementCount (){
+        //     this.count++
+        // }
+    },
+    components:{
+        'counter': Counter,
+        // 'counter-brother': CounterBrother
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ todomvc/vuex_counter/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">   
+    click {{$store.state.count}} times, count is {{evenOrOdd}}
+    {{$store.state.count}}
+    <button @click="increment"> +</button>
+    <button @click="decrement"> -</button>
+    <button @click="incrementIfOdd">increment If Odd</button> 
+    <button @click="incrementAsync">Increment Async</button>
+  </div>
+</template>
+
+<script>
+  // 借
+  import { mapActions, mapGetters } from 'vuex'
+  export default {
+    name: 'App',
+    methods: mapActions([
+      'increment',
+      'decrement',
+      'incrementIfOdd',
+      'incrementAsync'
+    ]),
+  components: {
+
+  },
+  computed:
+    // 如果有多处要用到奇偶判断怎么办
+    mapGetters([
+      'evenOrOdd'
+  ])
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
