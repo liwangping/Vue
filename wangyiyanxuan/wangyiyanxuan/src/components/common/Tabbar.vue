a227964778b7d58bc3b6e58edf6abe0ddc5cfc41
lwpwyf <1012312786@qq.com>
2019-07-05 22:21:10 +0800

    wangyiyanxuan

---
+++ wangyiyanxuan/wangyiyanxuan/src/components/common/Tabbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        底部导航栏
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabbar',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ wangyiyanxuan/wangyiyanxuan/src/components/pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <tabbar></tabbar>
+    </div>
+</template>
+
+<script>
+import tabbar from "../common/Tabbar"
+export default {
+    name: 'Home',
+    data () {
+        return {
+        }
+    },
+    components:{
+        tabbar
+    }
+}
+</script>
+
+<style>
+
+</style>
